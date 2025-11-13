1642e3fac0dcc063a714dd17920effdcf50aa681
Tom <tom-dev@abracode.com>
2025-11-12 17:39:38 -0800

    Change codesign_app.sh to allow ad-hoc codesigning
    Adding a second param to `codesign_app.sh` to provide signing identity.
    An identity can be created with a self-signed certificate in Keychain Access.app -> Certificate Assistant -> Create a Certificate.
    For non-AppStore distribution, an Apple-issued "Developer ID" certificate identity can be provided. Finally, if the param is omitted, the ad-hoc signing is valid only for current machine where the signing happened.
    ad-hoc codesigned app does not use hardened runtime or entitlements, and does not use timestamp (which is needed for distribution)

--- codesign_app.sh
+++ codesign_app.sh
@@ -1,36 +1,47 @@
 #!/bin/sh
 
 self_dir=$(/usr/bin/dirname "$0")
-
 app_to_sign="$1"
+identity="$2"
+
 if test -z "$app_to_sign"; then
-	echo "error: a path to app must be provided"
-	exit 1
+    echo "error: a path to app must be provided"
+    exit 1
 fi
 
 # full path
 app_to_sign=$(/bin/realpath "$app_to_sign")
-
 app_id=$(/usr/bin/defaults read "$app_to_sign/Contents/Info.plist" CFBundleIdentifier)
 if test "$?" != "0"; then
-	echo "error: could not obtain bundle identifier for app at: $app_to_sign"
-	exit 1
+    echo "error: could not obtain bundle identifier for app at: $app_to_sign"
+    exit 1
+fi
+
+entitlements_path="$self_dir/OMCApplet.entitlements"
+
+if test -z "$identity"; then
+    identity="-"
+    entitlements=""
+    timestamp="--timestamp=none"
+    options=""
+else
+    entitlements="--entitlements \"$entitlements_path\""
+    timestamp="--timestamp"
+    options="--options runtime"
 fi
 
 if test -d "$app_to_sign/Contents/Support/Llama.cpp"; then
-	pushd "$app_to_sign/Contents/Support/Llama.cpp"
-	echo "/usr/bin/codesign --verbose --force --options runtime --timestamp --sign T9NM2ZLDTY '*'"
-	/usr/bin/codesign --verbose --force --options runtime --timestamp --sign "T9NM2ZLDTY" *
-	popd
+    pushd "$app_to_sign/Contents/Support/Llama.cpp"
+    echo "/usr/bin/codesign --verbose --force $options $timestamp --sign $identity '*'"
+    /usr/bin/codesign --verbose --force $options $timestamp --sign "$identity" *
+    popd
 fi
 
-
-echo "/usr/bin/codesign --deep --verbose --force --options runtime --entitlements $self_dir/OMCApplet.entitlements --timestamp --identifier $app_id --sign T9NM2ZLDTY $app_to_sign"
-/usr/bin/codesign --deep --verbose --force --options runtime --entitlements "$self_dir/OMCApplet.entitlements" --timestamp --identifier "$app_id" --sign "T9NM2ZLDTY" "$app_to_sign"
+echo "/usr/bin/codesign --deep --verbose --force $options $entitlements $timestamp --identifier $app_id --sign $identity $app_to_sign"
+/usr/bin/codesign --deep --verbose --force $options $entitlements $timestamp --identifier "$app_id" --sign "$identity" "$app_to_sign"
 
 echo ""
 echo "Verifying codesigned app:"
 echo "-------------------------"
-codesign -dv --verbose=4 "$app_to_sign"
+/usr/bin/codesign -dv --verbose=4 "$app_to_sign"
 echo "-------------------------"
-
